c9b5864305c33de2450ee4a3a35641e2ffdee339
laubernal <lbernalrodrguez@gmail.com>
2022-05-27 13:13:49 +0200

    added scroll to top

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,13 +1,18 @@
-import { Container } from '@mantine/core';
 import React from 'react';
+import { ActionIcon, Affix, Button, Container, Transition } from '@mantine/core';
+import { useWindowScroll } from '@mantine/hooks';
+import { ArrowBarToUp } from 'tabler-icons-react';
+
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import Hero from '../components/hero/Hero';
 import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Skills from './Skills';
+import { primaryColor } from '../constants';
 
 function Home(): JSX.Element {
+  const [scroll, scrollTo] = useWindowScroll();
   return (
     <Container>
       <Header />
@@ -16,6 +21,15 @@ function Home(): JSX.Element {
       <Skills />
       <Projects />
       <Footer />
+      <Affix position={{ bottom: 30, right: 30 }}>
+        <Transition transition="slide-up" mounted={scroll.y > 0}>
+          {transitionStyles => (
+            <ActionIcon style={transitionStyles} onClick={() => scrollTo({ y: 0 })}>
+              <ArrowBarToUp size={35} strokeWidth={3} color={primaryColor} />
+            </ActionIcon>
+          )}
+        </Transition>
+      </Affix>
     </Container>
   );
 }
