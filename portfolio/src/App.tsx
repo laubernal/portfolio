7b73c60339c8dbe5085c0379367e0ddd5c7b96cc
laubernal <lbernalrodrguez@gmail.com>
2022-06-14 17:06:56 +0200

    added scroll to top when change route

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,15 +4,18 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import TrainingApp from './pages/TrainingApp';
 import ResourcesLinks from './pages/ResourcesLinks';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = (): JSX.Element => {
   return (
     <MantineProvider theme={{ fontFamily: 'Quicksand' }}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/training-app" element={<TrainingApp />} />
-        <Route path="/resources-links" element={<ResourcesLinks />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/training-app" element={<TrainingApp />} />
+          <Route path="/resources-links" element={<ResourcesLinks />} />
+        </Routes>
+      </ScrollToTop>
     </MantineProvider>
   );
 };
